5b41bb93ecd44cd15014ddbf8e28d727feea62bc
George Stavrinos <gstavrinos@iit.demokritos.gr>
2021-05-27 21:31:28 +0300

    Updated installation script

--- install.bash
+++ install.bash
@@ -9,6 +9,16 @@ v=$1
 
 cp -r $v/.arduino15 ~/.arduino15
 
+./$v/arduino-cli lib install JC_Button
+./$v/arduino-cli lib install "Adafruit NeoPixel"
+./$v/arduino-cli lib install SharpIR
+./$v/arduino-cli lib install AccelStepper
+./$v/arduino-cli lib install SD
+./$v/arduino-cli lib install "Adafruit VS1053 Library" 
+./$v/arduino-cli lib install SdFat
+./$v/arduino-cli lib install "VS1053 for use with SdFat"
+./$v/arduino-cli lib install MFRC522
+
 echo "alias arcuino-cli=$PWD/$v/arduino-cli" >> ~/.bashrc
 
 echo "Done!"
